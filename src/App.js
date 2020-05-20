61ce51c3df5745343e593bc4614900e099c36fca
bulbulahmed9 <bulbulahmed.tk@gmail.com>
2020-05-20 13:08:39 +0600

    react tag input implemented

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+import InputSkill from './InputSkill';
+
+function App() {
+  return (
+    <div className="App">
+      <InputSkill />
+    </div>
+  );
+}
+
+export default App;
